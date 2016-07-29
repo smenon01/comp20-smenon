0287a1876bba9a60791906adedec7075a7ebd125
smenon01 <suryammenon@gmail.com>
2016-07-29 10:45:12 -0400

    Add JS code

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,33 @@
 // Your JavaScript goes here...
+
+function parse()
+{
+	var doc = new XMLHttpRequest();
+	
+	doc.open("GET", "data.json", true);
+	//doc.open("GET",  "https://messagehub.herokuapp.com/messages.json", true);
+	
+	doc.onreadystatechange = callback;
+	
+	doc.send(null);
+	
+	function callback()
+	{
+		if (doc.readyState == 4 && doc.status == 200) {
+			var data = doc.responseText;
+			var parsed = JSON.parse(data);
+			var msg = ""
+      
+      		var old = document.getElementById("messages");
+      		for (var i = 0; i < parsed.length; i++) {
+      			//msg = '<p>' + parsed[i].content + " -" + parsed[i].username + '</p>';
+      			msg = '<p>' + parsed[i].content + " " + '<span id = "smaller">' + parsed[i].username + '</span>'  + '</p>';
+      			old.innerHTML += msg;
+      		}
+		}
+	
+		else if (doc.readyState == 4 && doc.status != 200) {
+			document.getElementById("messages").innerHTML = "<p>Fail</p>";
+		}
+	}
+}
